29050d16f76dd41ca6e3648cc1f4eac9ee673949
Emery Finkelstein <emery@emeryfinkelstein.ca>
2010-01-18 11:21:59 -0500

    All helpers included into ActiveRecord::Base instead of their respective modules. To ensure their availibility in views.

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
 # Include hook code here
 require File.dirname(__FILE__) + '/lib/conflict_warnings'
-ActionView::Helpers::UrlHelper.send :include, ConflictWarnings::ActionView::Helpers::UrlHelper
+ActionView::Base.send :include, ConflictWarnings::ActionView::Helpers::UrlHelper
 ActionController::Base.send :include, ConflictWarnings::ActionController::Base
-ActionView::Helpers::PrototypeHelper.send :include, ConflictWarnings::ActionView::Helpers::PrototypeHelper
-ActionView::Helpers::FormHelper.send :include, ConflictWarnings::ActionView::Helpers::FormHelper
+ActionView::Base.send :include, ConflictWarnings::ActionView::Helpers::PrototypeHelper
+ActionView::Base.send :include, ConflictWarnings::ActionView::Helpers::FormHelper
 
-ActionView::Helpers::FormTagHelper.send :include, ConflictWarnings::ActionView::Helpers::FormTagHelper
+ActionView::Base.send :include, ConflictWarnings::ActionView::Helpers::FormTagHelper
 
-ActionView::Helpers::FormBuilder.send :include, ConflictWarnings::ActionView::Helpers::FormBuilder 
+ActionView::Base.send :include, ConflictWarnings::ActionView::Helpers::FormBuilder 
 
 
